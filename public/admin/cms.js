1b8585dac6401231455382f61243c05e4bfc8f3c
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2021-04-11 22:37:52 +0530

    added _redirect file

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "14d378c75c679625f155";
+/******/ 	var hotCurrentHash = "a303eadf6bb12c75ac7f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -933,8 +933,8 @@ setImmediate(function () {
     return;
   }
 
-  reactHotLoader.register(_netlifyIdentityWidget, "_netlifyIdentityWidget", "C:\\Users\\Kartikey\\React Projects Udemy\\My personal projects\\Developers-domain-gatsby-blog\\node_modules\\gatsby-plugin-netlify-cms\\cms-identity.js");
-  reactHotLoader.register(addLoginListener, "addLoginListener", "C:\\Users\\Kartikey\\React Projects Udemy\\My personal projects\\Developers-domain-gatsby-blog\\node_modules\\gatsby-plugin-netlify-cms\\cms-identity.js");
+  reactHotLoader.register(_netlifyIdentityWidget, "_netlifyIdentityWidget", "C:\\Users\\Kartikey\\Personal Projects\\Developers-domain-gatsby-blog\\node_modules\\gatsby-plugin-netlify-cms\\cms-identity.js");
+  reactHotLoader.register(addLoginListener, "addLoginListener", "C:\\Users\\Kartikey\\Personal Projects\\Developers-domain-gatsby-blog\\node_modules\\gatsby-plugin-netlify-cms\\cms-identity.js");
 })();
 
 ;
@@ -1005,8 +1005,8 @@ _netlifyCmsApp.default.registerPreviewStyle("cms.css");
     return;
   }
 
-  reactHotLoader.register(_netlifyCmsApp, "_netlifyCmsApp", "C:\\Users\\Kartikey\\React Projects Udemy\\My personal projects\\Developers-domain-gatsby-blog\\node_modules\\gatsby-plugin-netlify-cms\\cms.js");
-  reactHotLoader.register(_emitter, "_emitter", "C:\\Users\\Kartikey\\React Projects Udemy\\My personal projects\\Developers-domain-gatsby-blog\\node_modules\\gatsby-plugin-netlify-cms\\cms.js");
+  reactHotLoader.register(_netlifyCmsApp, "_netlifyCmsApp", "C:\\Users\\Kartikey\\Personal Projects\\Developers-domain-gatsby-blog\\node_modules\\gatsby-plugin-netlify-cms\\cms.js");
+  reactHotLoader.register(_emitter, "_emitter", "C:\\Users\\Kartikey\\Personal Projects\\Developers-domain-gatsby-blog\\node_modules\\gatsby-plugin-netlify-cms\\cms.js");
 })();
 
 ;
@@ -1062,7 +1062,7 @@ module.exports = g;
     return;
   }
 
-  reactHotLoader.register(g, "g", "C:\\Users\\Kartikey\\React Projects Udemy\\My personal projects\\Developers-domain-gatsby-blog\\node_modules\\gatsby-plugin-netlify-cms\\node_modules\\webpack\\buildin\\global.js");
+  reactHotLoader.register(g, "g", "C:\\Users\\Kartikey\\Personal Projects\\Developers-domain-gatsby-blog\\node_modules\\gatsby-plugin-netlify-cms\\node_modules\\webpack\\buildin\\global.js");
 })();
 
 ;
@@ -1683,8 +1683,8 @@ exports.clearImmediate = (typeof self !== "undefined" && self.clearImmediate) ||
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Kartikey\React Projects Udemy\My personal projects\Developers-domain-gatsby-blog\node_modules\gatsby-plugin-netlify-cms\cms.js */"./node_modules/gatsby-plugin-netlify-cms/cms.js");
-module.exports = __webpack_require__(/*! C:\Users\Kartikey\React Projects Udemy\My personal projects\Developers-domain-gatsby-blog\node_modules\gatsby-plugin-netlify-cms\cms-identity.js */"./node_modules/gatsby-plugin-netlify-cms/cms-identity.js");
+__webpack_require__(/*! C:\Users\Kartikey\Personal Projects\Developers-domain-gatsby-blog\node_modules\gatsby-plugin-netlify-cms\cms.js */"./node_modules/gatsby-plugin-netlify-cms/cms.js");
+module.exports = __webpack_require__(/*! C:\Users\Kartikey\Personal Projects\Developers-domain-gatsby-blog\node_modules\gatsby-plugin-netlify-cms\cms-identity.js */"./node_modules/gatsby-plugin-netlify-cms/cms-identity.js");
 
 
 /***/ }),
